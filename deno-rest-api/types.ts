e445fd4bca90ead8f3e1e267c9868e458764fe9c
Pubudu  Dissanayake <u5857495@anu.edu.au>
2020-05-27 13:46:34 +1000

    System restraints encoded in types

--- deno-rest-api/types.ts
+++ deno-rest-api/types.ts
@@ -4,3 +4,26 @@ export interface Product {
   description: string;
   price: number;
 }
+
+// Bad: System restraints only encoded in the comments
+interface User {
+  /**
+   *  A user can have either a username or email, but never both
+   */
+  id: number;
+  username?: string;
+  email?: string;
+}
+
+// Good: System restraints encoded in types
+interface BaseUser {
+  id: number;
+}
+
+interface UserWithEmail extends BaseUser {
+  email: string;
+}
+
+interface UserWithUserName extends BaseUser {
+  username: string;
+}
